51816a19ba28d99aebc182345d9d719e99f97063
Ishaan Rawat <ishaan@ishaanrawat.com>
2023-06-18 14:40:37 +0530

    gtm

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,14 +6,14 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <Script
-          id="gtm-script"
-          strategy="afterInteractive"
+          id="gtm"
+          strategy="lazyOnload"
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-PZLG77S');`,
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-PZLG77S');`,
           }}
         />
       </Head>
