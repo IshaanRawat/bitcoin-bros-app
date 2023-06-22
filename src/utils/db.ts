02e480efd980c2c7bf2c6c4bcc057adbd4f10114
Ishaan Rawat <ishaan@ishaanrawat.com>
2023-06-22 15:54:18 +0530

    prevent multiple db connections

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -5,6 +5,7 @@ import config from "./../../config.json";
 let db: Db;
 
 export async function connectToDb() {
+  if (db) return;
   const client = new MongoClient(config.MONGO_URL);
   await client.connect();
   db = client.db(process.env.DB_NAME);
